7a9b8d164a68f65d97271fcbc85bc1b25882b7b1
Miguel Jiménez <69486469+migue3210@users.noreply.github.com>
2024-02-22 18:40:53 -0400

    Adds register page

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,13 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import Login from './pages/login/login'
+import Register from './pages/register/register'
 
 function App() {
 
   return (
     <>
-      <Login></Login>
+      <Register></Register>
     </>
   )
 }
