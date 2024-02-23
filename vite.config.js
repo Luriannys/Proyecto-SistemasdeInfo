cb037575010fae581f89597fa73ff296ee06baee
Miguel Jiménez <69486469+migue3210@users.noreply.github.com>
2024-02-22 20:01:47 -0400

    update pwa

--- vite.config.js
+++ vite.config.js
@@ -9,8 +9,8 @@ export default defineConfig({
     registerType: 'autoUpdate',
     includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'mask-icon.svg'],
     manifest: {
-      name: 'Vite PWA Project',
-      short_name: 'Vite PWA Project',
+      name: 'Agrupamet',
+      short_name: 'Agrupamet',
       theme_color: '#ffffff',
       icons: [
         {
