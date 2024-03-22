a7f51fd79af8d92595a7788321619f8fbe8487d3
Alexandra Lecuna <alecuna@correo.unimet.edu.ve>
2024-03-22 11:21:45 -0400

    Creacion page Agrupaciones con Route

--- src/App.jsx
+++ src/App.jsx
@@ -1,44 +1,25 @@
-import {
-  BrowserRouter as Router,
-  Routes,
-  Route
-} from "react-router-dom";
-import './App.css'
-import Login from './view/pages/login/login';
-import Register from './view/pages/register/register';
-import Landing from './view/pages/landing/landing';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import "./App.css";
+import Login from "./view/pages/login/login";
+import Register from "./view/pages/register/register";
+import Landing from "./view/pages/landing/landing";
 import Dashboard from "./view/pages/dashboard/dashboard";
+import Agrupacion from "./view/pages/agrupacion/agrupacion";
 
 function App() {
-
   return (
     <>
       <Router>
         <Routes>
-          <Route
-            exact
-            path="/"
-            element={<Landing />}
-          ></Route>
-          <Route
-            exact
-            path="/login"
-            element={<Login />}
-          ></Route>
-          <Route
-            exact
-            path="/register"
-            element={<Register />}
-          ></Route>
-          <Route
-            exact
-            path="/dashboard"
-            element={<Dashboard />}
-          ></Route>
+          <Route exact path="/" element={<Landing />}></Route>
+          <Route exact path="/login" element={<Login />}></Route>
+          <Route exact path="/register" element={<Register />}></Route>
+          <Route exact path="/dashboard" element={<Dashboard />}></Route>
+          <Route exact path="/agrupacion" element={<Agrupacion />}></Route>
         </Routes>
       </Router>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
