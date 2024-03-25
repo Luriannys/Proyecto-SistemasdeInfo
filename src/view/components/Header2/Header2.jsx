b5c6218a9bdb9ed3a154eab90ffc63f18d605c58
Alexandra Lecuna <alecuna@correo.unimet.edu.ve>
2024-03-25 08:06:14 -0500

    barra de busqueda en el menu de agrupaciones

--- src/view/components/Header2/Header2.jsx
+++ src/view/components/Header2/Header2.jsx
@@ -1,37 +1,50 @@
-import './Header2.css';
-import logo from '../../assets/unimetLogo.png';
-import person_icon from '../../assets/person.svg';
-import expand_more from '../../assets/expand_more.svg';
-import { useState } from 'react';
-import React from 'react'
-import { Link } from 'react-router-dom';
-
+import "./Header2.css";
+import logo from "../../assets/unimetLogo.png";
+import person_icon from "../../assets/person.svg";
+import expand_more from "../../assets/expand_more.svg";
+import { useState } from "react";
+import React from "react";
+import { Link } from "react-router-dom";
 
 const Header2 = () => {
   const [isOpen, setIsOpen] = useState(false);
   const toggleMenu = () => {
-    setIsOpen(!isOpen);};
+    setIsOpen(!isOpen);
+  };
 
   return (
-    <div className='bar'>
-        <img src={logo} alt="Logo Unimet" className="logo_" />
-        <input className='search' type='text' placeholder='Buscar Agrupaciones'/>
-        <div className='perfil'>
-        <div className='icon'><img src={person_icon}  alt='icono'/></div>
-        <button onClick={toggleMenu} className='btn_expand' type="button"> <img src={expand_more}/> </button>
+    <div className="bar">
+      <img src={logo} alt="Logo Unimet" className="logo_" />
+      {/* <input
+        className="search"
+        type="text"
+        placeholder="Buscar Agrupaciones"
+        onChange={(e) => setSearchQuery(e.target.value)}
+      /> */}
+      <div className="perfil">
+        <div className="icon">
+          <img src={person_icon} alt="icono" />
+        </div>
+        <button onClick={toggleMenu} className="btn_expand" type="button">
+          {" "}
+          <img src={expand_more} />{" "}
+        </button>
+      </div>
+      {isOpen && (
+        <div className="menu">
+          <Link to="/Perfil" className="l">
+            <button className="btn_p">Mi cuenta</button>
+          </Link>
+          <Link className="l">
+            <button className="btn_p">Realizar Donacion</button>
+          </Link>
+          <Link className="l">
+            <button className="btn_p">Cerrar Sesion</button>
+          </Link>
+        </div>
+      )}
     </div>
-    {isOpen && (
-          <div className='menu'>
-            <Link to='/Perfil' className='l'><button className='btn_p'>Mi cuenta</button></Link>
-            <Link className='l'><button className='btn_p'>Realizar Donacion</button></Link>
-            <Link className='l'><button className='btn_p'>Cerrar Sesion</button></Link>
-          </div>)}
-        
+  );
+};
 
-    </div>
-  )
-}
-
-export default Header2
-
-
+export default Header2;
