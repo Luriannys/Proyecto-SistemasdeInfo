d7164f3391cf64baaa4b5fc390df87a55bb87997
migue3210 <69486469+migue3210@users.noreply.github.com>
2024-03-30 17:50:47 -0400

    update

--- src/view/pages/agrupacion/agrupacion.jsx
+++ src/view/pages/agrupacion/agrupacion.jsx
@@ -8,12 +8,8 @@ import email from '../../assets/email.svg';
 import {
   doc,
   getDoc,
-  collection,
-  getDocs,
-  query,
-  where,
 } from "firebase/firestore";
-import React, { useState, useEffect } from "react";
+import { useState, useEffect } from "react";
 import { useParams, Link } from "react-router-dom";
 
 export default function Agrupacion() {
@@ -33,7 +29,7 @@ export default function Agrupacion() {
     };
 
     fetchAgrupacion();
-  }, []);
+  }, [id]);
 
   if (!agrupacion) {
     return <div></div>;
@@ -45,36 +41,33 @@ export default function Agrupacion() {
     <div>
       <Header2 />
       <div className="imagen_group">
-      <img className='foto' src={saman}></img>
-      <h1 className="title_g">{agrupacion.nombre}</h1>
+        <img className='foto' src={saman}></img>
+        <h1 className="title_g">{agrupacion.nombre}</h1>
       </div>
       <div className="Info_card">
-      <p style={{ textAlign: 'justify' }}>{agrupacion.descripcion}</p>
-      <div className="btns">
-      <Link to="/donaciones"><button className="btns_"> <strong>Donar </strong></button></Link>
-      <button className="btns_"> <strong>Agregar Testimonio</strong> </button>
-      <button className="btns_"> <strong>Unete </strong></button>
-      </div>
-      <div style={{ marginBottom: '15px' }}>
-      <div style={{ marginBottom: '15px'}}>
-      <strong>Contáctanos</strong>
-<<<<<<< Updated upstream
-=======
-      </div>
->>>>>>> Stashed changes
-        <div className="contact_us"><img className='icon_i' src={ig}/><p>{agrupacion.instagram}</p></div>
-        <div className="contact_us"><img className='icon_i' src={email}/><p>{agrupacion.email}</p></div>
-      </div>
-      <span style={{ color: 'black', fontSize: '40px', textAlign:'center' }}>Mira lo que <span style={{ color: '#FD8204', fontSize: '40px', textAlign:'center'}}>otros opinan</span></span>
-      <ul className="testimonies">
-        {agrupacion.testimonios.map((item) => (
-          <li style={{listStyleType: 'none'}} className="feedback_box">
-            <span >{item}</span>
-          </li>
-        ))}
-      </ul>
-      </div>
+        <p style={{ textAlign: 'justify' }}>{agrupacion.descripcion}</p>
+        <div className="btns">
+          <Link to="/donaciones"><button className="btns_"> <strong>Donar </strong></button></Link>
+          <button className="btns_"> <strong>Agregar Testimonio</strong> </button>
+          <button className="btns_"> <strong>Unete </strong></button>
+        </div>
+        <div style={{ marginBottom: '15px' }}>
+          <div style={{ marginBottom: '15px' }}>
+            <strong>Contáctanos</strong>
+          </div >
+          <div className="contact_us"><img className='icon_i' src={ig} /><p>{agrupacion.instagram}</p></div>
+          <div className="contact_us"><img className='icon_i' src={email} /><p>{agrupacion.email}</p></div>
+        </div >
+        <span style={{ color: 'black', fontSize: '40px', textAlign: 'center' }}>Mira lo que <span style={{ color: '#FD8204', fontSize: '40px', textAlign: 'center' }}>otros opinan</span></span>
+        <ul className="testimonies">
+          {agrupacion.testimonios.map((item) => (
+            <li key={item} style={{ listStyleType: 'none' }} className="feedback_box">
+              <span >{item}</span>
+            </li>
+          ))}
+        </ul>
+      </div >
       <Footer2></Footer2>
-    </div>
+    </div >
   );
 }
