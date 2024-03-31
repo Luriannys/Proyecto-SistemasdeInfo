c07a8cecf098901a067e989d6a8a39fd63573339
Luriannys <131465439+Luriannys@users.noreply.github.com>
2024-03-31 18:29:57 -0400

    Update Header2.jsx

--- src/view/components/Header2/Header2.jsx
+++ src/view/components/Header2/Header2.jsx
@@ -4,7 +4,6 @@ import person_icon from '../../assets/person.svg';
 // import plus from '../../assets/plus.svg'
 import expand_more from '../../assets/expand_more.svg';
 import { useState } from 'react';
-import React from 'react'
 import { Link } from 'react-router-dom';
 
 
@@ -15,21 +14,19 @@ const Header2 = () => {
   };
 
   return (
-    <div className={style.bar}>
-        <img src={logo} alt="Logo Unimet" className={style.logo} />
-        <div className={style.perfil}>
-        {/* <div className={style.plus}><img src={plus} alt= 'plus' /></div> */}
-        <div className={style.icon}><img src={person_icon}  alt='icono'/>
-        </div>
-        <button onClick={toggleMenu} className={style.btn_expand} type="button"> <img src={expand_more}/> </button>
+    <div className='bar'>
+        <img src={logo} alt="Logo Unimet" className="logo_" />
+        <div className='perfil'>
+        <div className='icon'><img src={person_icon}  alt='icono'/></div>
+        <button onClick={toggleMenu} className='btn_expand' type="button"> <img src={expand_more}/> </button>
     </div>
     {isOpen && (
-        <div className={style.menu}>
-          <Link to='/Perfil' className={style.l}><button className={style.btn_p}>Mi cuenta</button></Link>
-          <Link to='/donaciones' className={style.l}><button className={style.btn_p}>Realizar Donación</button></Link>
-          <Link className={style.l}><button className={style.btn_p}>Cerrar Sesión</button></Link>
+        <div className='menu'>
+          <Link to='/Perfil' className='l'><button className='btn_p'>Mi cuenta</button></Link>
+          <Link to='/donaciones' className='l'><button className='btn_p'>Realizar Donación</button></Link>
+          <Link className='l'><button className='btn_p'>Cerrar Sesion</button></Link>
         </div>)}
-        
+
 
     </div>
   );
