35f940668624e67ff7adced24c678d43959804f3
SamGRT <samantha.rojas@correo.unimet.edu.ve>
2024-03-31 17:56:11 -0400

    Se agrega vista de crear agrupacion

--- src/view/pages/New_Group/New_Group.jsx
+++ src/view/pages/New_Group/New_Group.jsx
@@ -1,9 +1,36 @@
 import React from 'react'
 import './New_Group.css';
+import Header2 from '../../components/Header2/Header2';
 
 const New_Group = () => {
   return (
-    <div>New_Group</div>
+        <>
+        <Header2/>
+        <h2 id='ti'>Crear agrupación</h2>
+        <div className='newG_container'>
+        <div className='c'>
+          <label >Nombre
+              <input className='input2' type='text' />
+          </label>
+          <label>Email
+              <input className='input2' type='email' />
+          </label>
+          <label>Instagram
+              <input className='input2' type='text' />
+          </label>
+          <label>Teléfono
+              <input className='input2' type='text' />
+          </label>
+          <label>Descripción
+              <input className='input2' type='text' />
+          </label>
+          <label>Imagen de la agrupación - URL
+              <input className='input2' type='text' />
+          </label>
+          </div>
+          <button className='btns_'>Crear Agrupación</button>
+              
+          </div></>
   )
 }
 
